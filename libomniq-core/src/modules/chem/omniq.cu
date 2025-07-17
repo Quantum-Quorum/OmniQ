2c812f26b120eae80d6aed4a7419a8e6a72c0644
gouthamarcot <gauthamarcot@yahoo.com>
2025-07-17 12:45:51 +0530

    added chem module

---
+++ libomniq-core/src/modules/chem/omniq.cu
@@ -0,0 +1,6 @@
+//
+// Created by Goutham Arcot on 17/07/25.
+//
+
+#include "omniq.cuh"
+
---
+++ libomniq-core/src/modules/chem/omniq.cuh
@@ -0,0 +1,8 @@
+//
+// Created by Goutham Arcot on 17/07/25.
+//
+
+#ifndef OMNIQ_CUH
+#define OMNIQ_CUH
+
+#endif //OMNIQ_CUH
